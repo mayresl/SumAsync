b1c56725c9513333bfca2510019ecc4ea3000c17
Marcela <you@example.com>
2023-06-20 11:41:26 -0300

    Calling node api

--- API/index.js
+++ API/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
  
 app.get('/GetResult', (req, res) => {
-  res.send('Hello World!')
+  res.send(JSON.stringify('Hello World!'))
 })
  
 app.post('/CalcAsync', (req, res) => {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CalculatorComponent } from './calculator/calculator.component';
 
+import { HttpClientModule } from '@angular/common/http'; 
+
 @NgModule({
   declarations: [
     CalculatorComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
